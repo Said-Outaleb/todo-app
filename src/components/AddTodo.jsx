7cb30381c403b6d3975b54947b31a7fe5a70493a
Said Outaleb <92954498+Sinkodatibayo@users.noreply.github.com>
2024-07-24 18:40:38 -0700

    Update AddTodo.jsx

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -13,7 +13,7 @@ export default function AddTodo({
       <input
         className={`w-full min-h-12
         px-2 py-4 placeholder:text-slate-600 outline-none rounded-md capitalize mt-1  ${
-          mode === light ? "bg-slate-100 text-slate-950" : ""
+          mode === light ? "bg-slate-100 text-slate-950" : "bg-slate-800"
         }`}
         placeholder="Enter a Todo..."
         value={clear}
